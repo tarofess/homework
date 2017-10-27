fbe5181d119534154d455d98c2a335076b46fb20
松崎太郎 <t-matsusaki@interpia.ne.jp>
2017-10-27 14:41:30 +0900

    CocoaPodからCarthageに切り替え

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, ’10.0’
-use_frameworks!
-
-target ‘宿題マスター2’ do
-pod 'RealmSwift'
-pod 'Firebase/Core'
-pod 'Firebase/AdMob'
-end
--- Pods/Target Support Files/Pods-宿題マスター2/Pods-宿題マスター2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_______2 : NSObject
-@end
-@implementation PodsDummy_Pods_______2
-@end
